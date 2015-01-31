2bbdd8c6573127fec31cf1fbd0bf5b266a137d7e
acdhound <profgansmok@gmail.com>
2015-01-31 23:07:24 +0300

    change cart.js, template_style.css

--- esign/esign/static/js/cart.js
+++ esign/esign/static/js/cart.js
@@ -1,17 +1,17 @@
+//изменение содержимого корзины
 function addtocart (prod_id, prod_num, prod_name)
 {
 	var items = $("#cart #orders_list .item");
-	var new_text, new_num, id_check = 0;
+	var new_num = 0;
+	var id_check = 0;
 	items.each(function ()
 		{
 			cur_div = $(this).attr("id").split('-');
 			if (cur_div[0] == prod_id)
 			{
 				new_num = parseInt(cur_div[1])+prod_num;
-				new_text = prod_name+" - "+new_num+"шт.";
 				$(this).attr({"id" : prod_id+"-"+new_num});
-				if ($(this).find(".order_txt").length == 0) {alert(".order_txt not found!");}
-				$(this).find(".order_txt").html(new_text);
+				$(this).find(".order_cnt").attr({"value" : new_num});
 				id_check = 1;
 			}
 		}
@@ -19,13 +19,17 @@ function addtocart (prod_id, prod_num, prod_name)
 	if (id_check == 0)
 	{
 	  	var new_item = $("<div>", {"id" : prod_id+"-"+prod_num, "class" : "item"}).appendTo("#cart #orders_list");
-	  	$("<div>", {"class" : "order_txt", "text" : prod_name+" - "+prod_num+"шт."}).appendTo(new_item);
-	  	$("<div>", {"class" : "remove_order", "text" : "убрать"}).appendTo(new_item);
+	  	new_item.load("../../templates/esign/cart_item_form.html");
+	  	setTimeout(function () {
+	  		new_item.find(".order_cnt").attr({"value" : prod_num});
+	  	    new_item.find(".order_txt").html(prod_name);
+	  	}, 50);
 	}
 }
 $(document).ready(function ()
 	{
 		var product_names_array = {fiz : "подпись физ. лица", yur : "подпись юр. лица", ip : "подпись ИП"};
+//клик в одной из форм раздела "оформить заказ"
 		$("#products_list .product input:button").click(function ()
 			{
 				var prod_id = $(this).parent().parent().attr("id").split('_')[0];
@@ -34,21 +38,44 @@ $(document).ready(function ()
 				{
 					prod_name = product_names_array[prod_id];
 					addtocart(prod_id, prod_num, prod_name);
+//------------------------------------------------------------					
 					send = prod_id+":+"+prod_num;
 				}
 				else
 				{
-					send = prod_id+":+0";
+//------------------------------------------------------------					
+					send = "";
 				}
 			}
 		);
+//клик на кнопку "убрать" в одном из элементов корзины
 		$("#orders_list").on("click", ".item .remove_order", function ()
 			{
-				$(this).parent().remove();
-				var prod_to_remove = $(this).parent().attr("id").split("-");
+				var prod_to_remove = $(this).parent().parent().attr("id").split("-");
 				var prod_id = prod_to_remove[0];
 				var prod_num = prod_to_remove[1];
+//------------------------------------------------------------				
 				var send = prod_id+":-"+prod_num;
+				$(this).parent().parent().remove();
+			}
+		);
+//клик на кнопку "поменять" в одном из элементов корзины
+		$("#orders_list").on("click", ".item .change_cnt", function ()
+			{
+				var new_cnt = $(this).parent().find(".order_cnt").val();
+				var send;
+				if ((parseInt(new_cnt)) && (new_cnt > 0))
+				{
+					var item_id = $(this).parent().parent().attr("id").split()[0];
+					$(this).parent().parent().attr({"id" : item_id+"-"+new_cnt});
+//------------------------------------------------------------
+					send = item_id+":"+new_cnt;
+				}
+				else
+				{
+//------------------------------------------------------------					
+					send="";
+				}
 			}
 		);
 	}
