61fc6c5c3f2c60c72a21933b61046a05b42694ae
Viktor <vhex@yandex.ru>
2015-02-12 04:31:16 +0300

    Updated scripts to manage data

--- esign/esign/accessory/db_manager.py
+++ esign/esign/accessory/db_manager.py
@@ -1,1 +1,6 @@
-__author__ = 'root'
+import traceback
+import django
+from django.db.transaction import set_autocommit, commit, rollback
+
+from esign.models import Types, Attributes, Entities, Parameters
+set_autocommit(False)
